7651292166b022eaf746373ecf5b266559c23a83
JoshJAllison <Joshua.Allison25@outlook.com>
2025-12-08 12:31:55 +0000

    feat: Implement CRUD for Account model

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,26 +1,31 @@
-﻿namespace ConnectCRM.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConnectCRM.Models
 {
     public class Account
     {
         public int Id { get; set; }
 
-        
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Required]
         public string Industry { get; set; } = string.Empty;
-        public string Website { get; set; } = string.Empty;
-        public string Phone { get; set; } = string.Empty;
+        public string? Website { get; set; }
+        public string? Phone { get; set; }
 
-        
+        [Required]
         public string Street { get; set; } = string.Empty;
+        [Required]
         public string City { get; set; } = string.Empty;
+        [Required]
         public string State { get; set; } = string.Empty;
+        [Required]
         public string PostalCode { get; set; } = string.Empty;
+        [Required]
         public string Country { get; set; } = string.Empty;
 
         
         public List<Contact> Contacts { get; set; } = new();
-
-        
         public List<Opportunity> Opportunities { get; set; } = new();
     }
 }
